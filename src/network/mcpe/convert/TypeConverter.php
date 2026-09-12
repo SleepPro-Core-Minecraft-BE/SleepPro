0423eaf6b3e672dac31df594a3cea15b43320e3c
thezeqa <118455573+ka1larness@users.noreply.github.com>
2026-09-12 13:26:26 +0300

    Encode modern recipe inputs by string ID

--- src/network/mcpe/convert/TypeConverter.php
+++ src/network/mcpe/convert/TypeConverter.php
@@ -50,6 +50,7 @@ use pocketmine\nbt\TreeRoot;
 use pocketmine\nbt\UnexpectedTagTypeException;
 use pocketmine\network\mcpe\NetworkBroadcastUtils;
 use pocketmine\network\mcpe\protocol\ClientboundPacket;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\network\mcpe\protocol\serializer\ItemTypeDictionary;
 use pocketmine\network\mcpe\protocol\types\GameMode as ProtocolGameMode;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStack;
@@ -159,9 +160,13 @@ class TypeConverter{
 			$oldStringId = $ingredient->getItemId();
 			[$stringId, $meta] = $this->itemDataDowngrader->downgrade($oldStringId, 0);
 
-			$id = $this->itemTypeDictionary->fromStringId($stringId);
 			$meta = $meta === 0 && $stringId === $oldStringId ? self::RECIPE_INPUT_WILDCARD_META : $meta; // downgrader returns the same meta
-			$descriptor = new IntIdMetaItemDescriptor($id, $meta);
+			if($this->protocolId >= ProtocolInfo::PROTOCOL_1_26_40){
+				$descriptor = new StringIdMetaItemDescriptor($stringId, $meta);
+			}else{
+				$id = $this->itemTypeDictionary->fromStringId($stringId);
+				$descriptor = new IntIdMetaItemDescriptor($id, $meta);
+			}
 		}elseif($ingredient instanceof ExactRecipeIngredient){
 			$item = $ingredient->getItem();
 			[$id, $meta, $blockRuntimeId] = $this->itemTranslator->toNetworkId($item);
@@ -171,7 +176,11 @@ class TypeConverter{
 					throw new AssumptionFailedError("Every block state should have an associated meta value");
 				}
 			}
-			$descriptor = new IntIdMetaItemDescriptor($id, $meta);
+			if($this->protocolId >= ProtocolInfo::PROTOCOL_1_26_40){
+				$descriptor = new StringIdMetaItemDescriptor($this->itemTypeDictionary->fromIntId($id), $meta);
+			}else{
+				$descriptor = new IntIdMetaItemDescriptor($id, $meta);
+			}
 		}elseif($ingredient instanceof TagWildcardRecipeIngredient){
 			$descriptor = new TagItemDescriptor($ingredient->getTagName());
 		}else{
